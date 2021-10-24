af1646c72ca9575a47debffb897d056ed363f5ae
Kostianyn <odessavshok@gmail.com>
2021-10-24 22:28:12 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -3,30 +3,25 @@
 //     а принимала всего два параметра allUsers и userName и возвращала промис.
 
 const users = [
-  { name: "Mango", active: true },
-  { name: "Poly", active: false },
-  { name: "Ajax", active: true },
-  { name: "Lux", active: false },
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map((user) =>
-    user.name === userName ? { ...user, active: !user.active } : user
-  );
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
 
-  callback(updatedUsers);
+    resolve(updatedUsers);
+  });
 };
-
-const logger = (updatedUsers) => console.table(updatedUsers);
-
-/*
- * Сейчас работает так
- */
-toggleUserState(users, "Mango", logger);
-toggleUserState(users, "Lux", logger);
+const logger = updatedUsers => console.table(updatedUsers);
 
 /*
  * Должно работать так
  */
-toggleUserState(users, "Mango").then(logger);
-toggleUserState(users, "Lux").then(logger);
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
